f3aed1828ba755ef9dfcf70ef050917c0387f33b
raza-altaf <info@mohammedaltaf.in>
2023-12-10 00:17:50 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect(
+  "mongodb+srv://4mohammedaltaf:w44Edv3SOxCW1YUc@ecommerceapi.mggf49a.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
+});
+
+module.exports = db;
